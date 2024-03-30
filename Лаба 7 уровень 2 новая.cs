69b61e49f01535d0052af5f6c9e79f234a04fa50
KirchuKsenia <159530394+KirchuKsenia@users.noreply.github.com>
2024-03-30 14:02:44 +0300

    Добавила исправленные задачи лабораторной 7

---
+++ Лаба 7 уровень 2 новая.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class People
+{
+    private string _name;
+    private string _surname;
+
+    public People(string name, string surname)
+    {
+        _name = name;
+        _surname = surname;
+    }
+
+    public string Name => _name;
+    public string Surname => _surname;
+}
+
+public class ChessPlayer : People
+{
+    private static int _idCounter = 1;
+    private int _id;
+    private double _finalScore;
+
+    public ChessPlayer(string name, string surname, int wins, double draws, int losses) : base(name, surname)
+    {
+        _id = _idCounter++;
+        _finalScore = wins + draws / 2;
+    }
+
+    public int Id => _id;
+    public double FinalScore => _finalScore;
+
+    public void Print()
+    {
+        Console.WriteLine($"{Name}\t{Surname}\t{_id}\t{_finalScore}");
+    }
+}
+
+internal class Program
+{
+    static void Sort(ChessPlayer[] participants)
+    {
+        for (int i = 0; i < participants.Length - 1; i++)
+        {
+            for (int j = i + 1; j < participants.Length; j++)
+            {
+                if (participants[j].FinalScore > participants[i].FinalScore)
+                {
+                    ChessPlayer temp = participants[j];
+                    participants[j] = participants[i];
+                    participants[i] = temp;
+                }
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        ChessPlayer[] participants = new ChessPlayer[]
+        {
+            new ChessPlayer("Ivan", "Ptichin", 3, 7, 0),
+            new ChessPlayer("Sergei", "Zhuravlev", 1, 4, 5),
+            new ChessPlayer("Viktor", "Abricosov", 6, 2, 2),
+            new ChessPlayer("Alexei", "Volcov", 7, 0, 3),
+            new ChessPlayer("Dmitri", "Sobakevich", 10, 0, 0)
+        };
+
+        Sort(participants);
+
+        Console.WriteLine("ФИО\t\tИД\tИтоговый балл");
+
+        foreach (ChessPlayer player in participants)
+        {
+            player.Print();
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ лаба 7 уровень 3 новая.cs
@@ -0,0 +1,129 @@
+﻿public class Participant
+{
+    private string _firstName; private string _lastName;
+    private int[] _exams; protected double _averageScore;
+    public Participant(string firstName, string lastName, int[] exams)
+    {
+        _firstName = firstName;
+        _lastName = lastName; _exams = exams;
+        _averageScore = CalculateAverageScore();
+    }
+    public double CalculateAverageScore()
+    {
+        int sum = 0;
+        foreach (int exam in _exams)
+        {
+            sum += exam;
+        }
+        return (double)sum / _exams.Length;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"{_lastName} {_firstName} {_averageScore}");
+    }
+}
+public class Group
+{
+    protected Participant[] _participants;
+    protected double _avarageScore;
+    public Group(Participant[] participants)
+    {
+        _participants = participants;
+        _avarageScore = CalculateAverageScore();
+    }
+    public virtual double CalculateAverageScore()
+    {
+        double totalScore = 0;
+        foreach (Participant participant in _participants)
+        {
+            totalScore += participant.CalculateAverageScore();
+        }
+        return totalScore / _participants.Length;
+    }
+    public virtual void Print()
+    {
+        for (int i = 0; i < _participants.Length; i++)
+        {
+            _participants[i].Print();
+
+        }
+        Console.Write(" " + _avarageScore);
+        Console.WriteLine();
+    }
+}
+public class GroupA : Group
+{
+    public GroupA(Participant[] participants) : base(participants) { }
+    public override void Print()
+    {
+        for (int i = 0; i < _participants.Length; i++)
+        {
+            _participants[i].Print();
+
+        }
+        Console.Write("A " + _avarageScore);
+        Console.WriteLine();
+    }
+}
+public class GroupB : Group
+{
+    public GroupB(Participant[] participants) : base(participants) { }
+    public override void Print()
+    {
+        for (int i = 0; i < _participants.Length; i++)
+        {
+            _participants[i].Print();
+
+        }
+        Console.Write("B " + _avarageScore);
+        Console.WriteLine();
+    }
+}
+public class GroupC : Group
+{
+    public GroupC(Participant[] participants) : base(participants) { }
+    public override void Print()
+    {
+        for (int i = 0; i < _participants.Length; i++)
+        {
+            _participants[i].Print();
+
+        }
+        Console.Write("C " + _avarageScore);
+        Console.WriteLine();
+    }
+}
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Participant[] participantsA = new Participant[]        
+        {
+            new Participant("Ivan", "Ptichin", new int[] { 78, 67, 56, 90 }),
+            new Participant("Sergei", "Zhuravlev", new int[] { 64, 89, 51, 99 }),
+        };
+
+        Participant[] participantsB = new Participant[]        
+        {
+            new Participant("Stepa", "Pupkin", new int[] { 78, 67, 56, 90 }),
+            new Participant("Dmitri", "Lebedev", new int[] { 64, 89, 51, 99 }),
+        };
+        Participant[] participantsC = new Participant[]        
+        {
+            new Participant("Lesha", "Sitnikov", new int[] { 78, 67, 56, 90 }),
+            new Participant("Sergei", "Samaev", new int[] { 64, 89, 51, 99 }),
+        };
+        Group gr = new Group(participantsC);
+        GroupA groupA = new GroupA(participantsA);
+        GroupB groupB = new GroupB(participantsB);
+        GroupC groupC = new GroupC(participantsC);
+        Group[] groups = new Group[] { groupA, groupB, groupC };
+
+        Console.WriteLine("Группа\tСредний балл за сессию");
+        for (int i = 0; i < groups.Length; i++)
+        {
+            groups[i].Print();
+        }
+        Console.ReadKey();
+    }
+}
